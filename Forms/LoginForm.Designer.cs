ba4c7f3da8f3508d62e7c0f7cafcbeef5a0bfd01
D3FAU4T <d3fau4t@gmail.com>
2023-05-04 22:08:56 +0530

    Organized repository

--- Forms/LoginForm.Designer.cs
+++ Forms/LoginForm.Designer.cs
@@ -36,7 +36,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Play", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label1.Location = new System.Drawing.Point(177, 76);
+            this.label1.Location = new System.Drawing.Point(184, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(322, 37);
             this.label1.TabIndex = 0;
