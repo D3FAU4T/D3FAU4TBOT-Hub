584097d0583615eb68aee3982dfb2d987e542601
D3FAU4T <d3fau4t@gmail.com>
2023-05-06 08:46:16 +0530

    Added Update Download option

--- Forms/SettingForm.Designer.cs
+++ Forms/SettingForm.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.CheckForUpdatesButton = new System.Windows.Forms.Button();
             this.UpdateHeader = new System.Windows.Forms.Label();
             this.UpdateText = new System.Windows.Forms.Label();
             this.ChangelogHeader = new System.Windows.Forms.Label();
             this.ChangelogTextBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // CheckForUpdatesButton
             // 
-            this.button1.Location = new System.Drawing.Point(53, 120);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Check for updates";
-            this.button1.UseVisualStyleBackColor = true;
+            this.CheckForUpdatesButton.BackColor = System.Drawing.Color.Fuchsia;
+            this.CheckForUpdatesButton.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.CheckForUpdatesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckForUpdatesButton.ForeColor = System.Drawing.Color.White;
+            this.CheckForUpdatesButton.Location = new System.Drawing.Point(53, 120);
+            this.CheckForUpdatesButton.Name = "CheckForUpdatesButton";
+            this.CheckForUpdatesButton.Size = new System.Drawing.Size(107, 23);
+            this.CheckForUpdatesButton.TabIndex = 0;
+            this.CheckForUpdatesButton.Text = "Check for updates";
+            this.CheckForUpdatesButton.UseVisualStyleBackColor = false;
+            this.CheckForUpdatesButton.Click += new System.EventHandler(this.CheckForUpdatesButton_Click);
             // 
             // UpdateHeader
             // 
@@ -78,14 +83,14 @@
             // 
             // ChangelogTextBox
             // 
+            this.ChangelogTextBox.BackColor = System.Drawing.Color.Blue;
+            this.ChangelogTextBox.ForeColor = System.Drawing.Color.White;
             this.ChangelogTextBox.Location = new System.Drawing.Point(53, 207);
             this.ChangelogTextBox.Name = "ChangelogTextBox";
             this.ChangelogTextBox.ReadOnly = true;
             this.ChangelogTextBox.Size = new System.Drawing.Size(592, 217);
             this.ChangelogTextBox.TabIndex = 4;
-            this.ChangelogTextBox.Text = "# Version 0.1.0 - May 1, 2022\n- Added feature 1\n- Added feature 2\n- Fixed bug 1\n\n" +
-    "# Version 0.2.0 - June 1, 2022\n- Added feature 3\n- Fixed bug 2\n- Improved perfor" +
-    "mance";
+            this.ChangelogTextBox.Text = "";
             // 
             // SettingForm
             // 
@@ -97,7 +102,7 @@
             this.Controls.Add(this.ChangelogHeader);
             this.Controls.Add(this.UpdateText);
             this.Controls.Add(this.UpdateHeader);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CheckForUpdatesButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SettingForm";
             this.Text = "SettingForm";
@@ -108,7 +113,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button CheckForUpdatesButton;
         private System.Windows.Forms.Label UpdateHeader;
         private System.Windows.Forms.Label UpdateText;
         private System.Windows.Forms.Label ChangelogHeader;
--- Forms/SettingForm.cs
+++ Forms/SettingForm.cs
@@ -1,51 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Octokit;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace D3FAU4TBOT_Hub.Forms
 {
     public partial class SettingForm : Form
-    {
-        public SettingForm()
+    {        
+        private bool UpdateAvailableBool = false;
+        private Release GlobalRelease;
+
+        public SettingForm(bool IsUpdateAvailable, Release LatestRelease)
         {
             InitializeComponent();
+            UpdateAvailableBool = IsUpdateAvailable;
+            GlobalRelease = LatestRelease;
+            UpdateAvailable(IsUpdateAvailable, LatestRelease);
             SetRichText();
         }
 
         private void SetRichText()
         {
-            // Load the changelog from a resource or file
-            string changelog =
-                "# Version 0.1.0 - May 1, 2022\r\n- Added feature 1\r\n- Added feature 2\r\n- Fixed bug 1\r\n\r\n# Version 0.2.0 - June 1, 2022\r\n- Added feature 3\r\n- Fixed bug 2\r\n- Improved performance";
+            string Changelog = File.ReadAllText(Path.Combine(System.Windows.Forms.Application.StartupPath, "Assets\\Changelog.md"));
+            ChangelogTextBox.Text = Changelog;
 
-            // Set the text of the RichTextBox control to the changelog
-            ChangelogTextBox.Text = changelog;
-
-            // Format the text as needed, for example, make the version numbers bold
-            foreach (Match match in Regex.Matches(changelog, @"^\s*#(.+)$", RegexOptions.Multiline))
+            foreach (Match match in Regex.Matches(Changelog, @"^\s*#(.+)$", RegexOptions.Multiline))
             {
                 int startIndex = match.Index;
                 int length = match.Length;
                 ChangelogTextBox.SelectionStart = startIndex;
                 ChangelogTextBox.SelectionLength = length;
                 ChangelogTextBox.SelectionFont = new Font(ChangelogTextBox.Font, FontStyle.Bold);
-                ChangelogTextBox.SelectionColor = Color.Blue;
+                ChangelogTextBox.SelectionColor = Color.LimeGreen;
             }
 
-            // Format the list items as bullet points
-            foreach (Match match in Regex.Matches(changelog, @"^- (.+)$", RegexOptions.Multiline))
+            foreach (Match match in Regex.Matches(Changelog, @"^- (.+)$", RegexOptions.Multiline))
             {
                 int startIndex = match.Index;
                 ChangelogTextBox.SelectionStart = startIndex;
                 ChangelogTextBox.SelectionLength = 1;
                 ChangelogTextBox.SelectionBullet = true;
+            }
+        }
+
+        private void UpdateAvailable(bool UpdateAvailable, Release LatestRelease)
+        {
+            if (UpdateAvailable)
+            {
+                CheckForUpdatesButton.Text = "Download";
+                UpdateText.Text = "Update available. Please download latest version from https://github.com/D3FAU4T/D3FAU4TBOT-Hub/releases";
+                File.WriteAllText(Path.Combine(System.Windows.Forms.Application.StartupPath, "Assets\\Changelog.md"), LatestRelease.Body);
+                SetRichText();
+            }
+
+            else
+            {
+                UpdateText.Text = "D3FAU4TBOT Hub is already up to date";
+                CheckForUpdatesButton.Text = "Check for updates";
+            }
+        }
+
+        private void CheckForUpdatesButton_Click(object sender, System.EventArgs e)
+        {
+            if (UpdateAvailableBool)
+            {
+                Process.Start("https://github.com/D3FAU4T/D3FAU4TBOT-Hub/releases");
+            }
+
+            else
+            {
+                Config ConfigData = new Config();
+                if (GlobalRelease.TagName.Replace("v", "") == ConfigData.Version)
+                {
+                    UpdateText.Text = "D3FAU4TBOT Hub is already up to date";
+                };
             }
         }
     }
