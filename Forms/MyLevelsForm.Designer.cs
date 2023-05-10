4199cd15ac6a2188a50895fab21bbc67c2a585f6
D3FAU4T <d3fau4t@gmail.com>
2023-05-10 22:35:35 +0530

    Added Level Browsing capability

---
+++ Forms/MyLevelsForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace D3FAU4TBOT_Hub.Forms
+{
+    partial class MyLevelsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LevelName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ExplorerBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // LevelName
+            // 
+            this.LevelName.Text = "LevelName";
+            this.LevelName.Width = 100;
+            // 
+            // ExplorerBrowser
+            // 
+            this.ExplorerBrowser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ExplorerBrowser.Location = new System.Drawing.Point(12, 71);
+            this.ExplorerBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.ExplorerBrowser.Name = "ExplorerBrowser";
+            this.ExplorerBrowser.Size = new System.Drawing.Size(690, 408);
+            this.ExplorerBrowser.TabIndex = 0;
+            // 
+            // MyLevelsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(714, 491);
+            this.Controls.Add(this.ExplorerBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MyLevelsForm";
+            this.Text = "MyLevelsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColumnHeader LevelName;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.WebBrowser ExplorerBrowser;
+    }
+}
---
+++ Forms/MyLevelsForm.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D3FAU4TBOT_Hub.Forms
+{
+    public partial class MyLevelsForm : Form
+    {
+        private static string MainFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "D3FAU4TBOT Hub");
+        private static string LevelFolder = Path.Combine(MainFolder, "My Levels");
+
+        public MyLevelsForm()
+        {
+            InitializeComponent();
+            InitializeLevels();
+        }
+
+        private async void InitializeLevels()
+        {
+            var Data = await GetLevelAsync(GetDiscordId());
+            DeserializeArrayAndWrite(Data);
+            ExplorerBrowser.Url = new Uri(LevelFolder);
+        }
+
+        private string GetDiscordId()
+        {
+            string ConfigFileText = File.ReadAllText(Path.Combine(MainFolder, "Config.json"));
+            Config ConfigFile = JsonConvert.DeserializeObject<Config>(ConfigFileText);
+            return ConfigFile.DiscordID;
+        }
+
+        private static async Task<LevelStructure[]> GetLevelAsync(string DiscordId)
+        {
+            using (HttpClient HttpClient = new HttpClient())
+            {
+                HttpResponseMessage Response = await HttpClient.GetAsync($"https://wos-level-editor.d3fau4tbot.repl.co/d3fau4tbot/getlevels/{DiscordId}");
+                if (Response.IsSuccessStatusCode)
+                {
+                    var Content = await Response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<LevelStructure[]>(Content);
+                }
+
+                return null;
+            }
+        }
+
+        private void DeserializeArrayAndWrite(LevelStructure[] LevelArray)
+        {
+            foreach (var Level in LevelArray)
+            {
+                Level.fakeLetters = "";
+                Level.hiddenLetters = "";
+                Level.reveal = false;
+                Level.level = "1";
+                Level.lang = Level.Language;
+                Level.Language = null;
+                Level.timebar = new Timebar
+                {
+                    timerPercentage = 100,
+                    locks = new Locks
+                    {
+                        total = 5,
+                        expired = 0
+                    }
+                };
+                string JSON = JsonConvert.SerializeObject(Level, Formatting.Indented);
+                string FilePath = Path.Combine(LevelFolder, $"{Level.Level}.level");
+                File.WriteAllText(FilePath, JSON);
+            }
+        }
+    }
+}
